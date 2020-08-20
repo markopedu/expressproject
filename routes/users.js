35d50d77953fb46baaa4bc1d82bac13622ed7e4b
markopedu <69790644+markopedu@users.noreply.github.com>
2020-08-20 15:15:37 +0200

    Create User

--- routes/users.js
+++ routes/users.js
@@ -14,10 +14,39 @@ router.get('/',  async function (req, res, next) {
 });
 
 router.get('/:id', async function(req, res, next) {
-  const id = req.params.id;
+
+  const { id } = req.params;
+
+  if(typeof id !== "number" ||  id === 'create') {
+    next();
+  }
+
   const user = await model.User.findById(id);
 
   res.render('user', { title: 'The User', user, id });
+});
+
+router.get('/create', async function(req, res, next) {
+   res.render('createuser', {  title: 'Create User' });
+});
+
+router.post('/create', async function(req, res, next) {
+
+  const { username } = req.body;
+
+  const existingUser = model.User.findOne({ username: username });
+
+  if(existingUser) {
+     res.redirect('/users');
+  }
+
+  const user = new model.User({
+        username: username
+  });
+
+  await user.save();
+
+  res.redirect('/users');
 });
 
 module.exports = router;
