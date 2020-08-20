54b6f5c35bdf3a1ca42edf6f6f9a0f14b70dfe2b
markopedu <69790644+markopedu@users.noreply.github.com>
2020-08-20 14:49:43 +0200

    get user by id

--- routes/users.js
+++ routes/users.js
@@ -13,4 +13,11 @@ router.get('/',  async function (req, res, next) {
   res.render('users', { title: 'Users', users: users });
 });
 
+router.get('/:id', async function(req, res, next) {
+  const id = req.params.id;
+  const user = await model.User.findById(id);
+
+  res.render('user', { title: 'The User', user, id });
+});
+
 module.exports = router;
